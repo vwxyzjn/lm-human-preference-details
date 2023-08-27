4b659bd412fbcd4299e3cce1f336009fbe00f930
Costa Huang <costa.huang@outlook.com>
2023-08-27 21:18:26 +0000

    fix test cases

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,7 +3,7 @@ import subprocess
 
 def test_torch():
     subprocess.run(
-        "accelerate launch --num_processes 1 lm_human_preference_details/train_both_accelerate.py --reward.labels.num_train 4 --policy.ppo.total_episodes 8 --policy.ppo.local_batch_size 4 --policy.ppo.whiten_rewards False", 
+        "accelerate launch --num_processes 1 lm_human_preference_details/train_both_accelerate.py --reward.labels.num_train 4 --policy.ppo.total_episodes 8 --policy.ppo.local_batch_size 4 --policy.ppo.no_whiten_rewards", 
         shell=True,
         check=True,
     )
