1c5957f6da0f3165e38f800095c6420883be44a0
liutianlin0121 <tliu@jacobs-alumni.de>
2023-09-02 12:11:43 +0200

    fix test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,7 +3,7 @@ import subprocess
 
 def test_torch():
     subprocess.run(
-        "python lm_human_preference_details/train_both_accelerate.py --reward.task.query_dataset dummy --policy.task.query_dataset dummy --reward.labels.num_train 4 --reward.local_normalize_samples 4 --reward.rollout_batch_size 4 --policy.ppo.total_episodes 8 --policy.ppo.local_batch_size 4 --policy.ppo.no_whiten_rewards", 
+        "python lm_human_preference_details/train_both_accelerate.py --reward.task.query_dataset dummy --policy.task.query_dataset dummy --reward.labels.num_train 4 --reward.normalize_samples 4 --reward.rollout_batch_size 4 --policy.ppo.total_episodes 8 --policy.ppo.local_batch_size 4 --policy.ppo.no_whiten_rewards", 
         shell=True,
         check=True,
     )
